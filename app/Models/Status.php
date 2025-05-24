4b6f8db66fde2f3c369b0bb8d0f1c03753003330
Zakhar Arkhipov <php-stager-1@turan-soft.ru>
2025-05-24 17:15:08 +0700

    Прописаны модели для тамблиц

---
+++ app/Models/Status.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Status extends Model
+{
+    protected $table = 'statuses';
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+}
---
+++ app/Models/Task.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    protected $table = 'tasks';
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
+
