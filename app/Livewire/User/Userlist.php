01153795752aad641ea14b66041325b49c2dc309
Zakhar Arkhipov <php-stager-1@turan-soft.ru>
2025-08-10 21:44:12 +0700

    Следуующий урок 4. Посмотрел 3 урок, узнал о загрузках, подтверждении удаления, о том как делать регистрацию пользователя, соответственно таким же образом получится сделать и авторизацию, узнал о существование magic функций

--- app/Livewire/User/Userlist.php
+++ app/Livewire/User/Userlist.php
@@ -3,23 +3,33 @@
 namespace App\Livewire\User;
 
 use Livewire\Component;
+use App\Models\User;
 
 class Userlist extends Component
 {
-    public string $title;
 
-    public string $username; 
+    public string $name; 
+    public string $email;
+    public string $password;
 
-    public array $users = [];
 
     public function addUser()
     {
-        $this->users[] = $this->username;
-        $this->reset(['username']);
+        User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => $this->password,
+        ]);
+        $this->reset(['name', 'email', 'password']);
+    }
+
+    public function deleteUser(int $id)
+    {
+        User::find($id)->delete();
     }
 
     public function render()
     {
-        return view('livewire.user.userlist');
+        return view('livewire.user.userlist')-> with(['users' => User::all()]);
     }
 }
--- resources/views/livewire/user/userlist.blade.php
+++ resources/views/livewire/user/userlist.blade.php
@@ -3,15 +3,38 @@
     <div class="col-md-6">
 
         <form wire:submit="addUser">
-        <input type="text" class="form-control" wire:model="username" wire:keydown.enter="addUser">
-        <button type="submit" class="btn btn-primary my-2">Добавить</button>
+        <div class="mb-3">
+            <input type="text" name="name" class="form-control" wire:model="name" wire:keydown.enter="addUser" placeholder="Имя">
+        </div>
+        <div class="mb-3">
+            <input type="email" class="form-control" wire:model="email" wire:keydown.enter="addUser" placeholder="Email">
+        </div>
+        <div class="mb-3">
+            <input type="password" class="form-control" wire:model="password" wire:keydown.enter="addUser" placeholder="Пароль">
+        </div>
+        <div class="d-flex align-items-center gap-3">
+            <button type="submit" class="btn btn-primary my-2">Добавить</button>
+            <div class="spinner-border text-primary" role="status" wire:loading wire:target="addUser">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
         </form>
     </div>
 
     <div class="col-md-6">
+
+        <div class="d-flex align-items-center gap-3">
+            <button wire:click="$refresh" class="btn btn-success">Обновить</button>
+            <div class="spinner-border text-success" role="status" wire:loading>
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+
         <ul>
             @forelse ($users as $user)
-                <li>{{ $user }}</li>
+                <li wire:key="{{ $user->id }}">{{ $user->name }} ({{ $user->email }})
+                    | <a href="#" wire:click.prevent="deleteUser({{ $user->id }})" wire:confirm="Удалить?">Удалить</a>
+                </li>
             @empty
                 <p>Тут пусто...</p>
             @endforelse
