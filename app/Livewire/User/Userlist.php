58ec0b67094b3fc16481aa44bc0be41342553974
Zakhar Arkhipov <php-stager-1@turan-soft.ru>
2025-08-13 22:31:21 +0700

    урок 5 пройден, я узнал про формы livewire

--- app/Livewire/User/Userlist.php
+++ app/Livewire/User/Userlist.php
@@ -2,46 +2,18 @@
 
 namespace App\Livewire\User;
 
+use App\Livewire\Forms\UserForm;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 use App\Models\User;
 
 class Userlist extends Component
 {
-    #[Validate('required')]
-    #[Validate('min:2', as: 'ЖОПА')]
-    #[Validate('max:30')]
-        public string $name;
-
-    #[Validate('required|email|max:30')]
-        public string $email;
-
-    #[Validate('required|min:6')]
-        public string $password;
-
-    
-    // protected function rules(): array
-    // {
-    //     return [
-    //         'name' => 'required|min:2|max:30', 
-    //         'email' => 'required|email|max:30',
-    //         'password' => 'required|min:6'
-    //     ];
-    // }
-
-    protected function messages()
-    {
-        return [
-            'name.required' => 'ИМЕЧКО ТА ОБЯЗАТЕЛЬНО'
-        ];
-    }
+    public UserForm $form;   
 
     public function addUser()
     {
-        $validated = $this->validate();
-
-        User::create($validated);
-        $this->reset();
+        $this->form->saveUser();
     }
 
     public function deleteUser(int $id)
--- resources/views/livewire/user/userlist.blade.php
+++ resources/views/livewire/user/userlist.blade.php
@@ -2,18 +2,25 @@
 
     <div class="col-md-6">
 
+        @if (session('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session('success') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+
         <form wire:submit="addUser">
         <div class="mb-3">
-            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" wire:model.blur="name" wire:keydown.enter="addUser" placeholder="Имя">
-            @error('name') <div class="invalid-feedback">{{ $message }}</div> @enderror
+            <input type="text" name="name" class="form-control @error('form.name') is-invalid @enderror" wire:model.blur="form.name" wire:keydown.enter="addUser" placeholder="Имя">
+            @error('form.name') <div class="invalid-feedback">{{ $message }}</div> @enderror
         </div>
         <div class="mb-3">
-            <input type="email" class="form-control @error('email') is-invalid @enderror" wire:model.blur="email" wire:keydown.enter="addUser" placeholder="Email">
-            @error('email') <div class="invalid-feedback">{{ $message }}</div> @enderror
+            <input type="email" class="form-control @error('form.email') is-invalid @enderror" wire:model.blur="form.email" wire:keydown.enter="addUser" placeholder="Email">
+            @error('form.email') <div class="invalid-feedback">{{ $message }}</div> @enderror
         </div>
         <div class="mb-3">
-            <input type="password" class="form-control @error('password') is-invalid @enderror" wire:model.blur="password" wire:keydown.enter="addUser" placeholder="Пароль">
-            @error('password') <div class="invalid-feedback">{{ $message }}</div> @enderror
+            <input type="password" class="form-control @error('form.password') is-invalid @enderror" wire:model.blur="form.password" wire:keydown.enter="addUser" placeholder="Пароль">
+            @error('form.password') <div class="invalid-feedback">{{ $message }}</div> @enderror
         </div>
         <div class="d-flex align-items-center gap-3">
             <button type="submit" class="btn btn-primary my-2">Добавить</button>
