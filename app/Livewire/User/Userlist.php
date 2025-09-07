ba64ba4cceb6495377a7c6c696dda1de1b5af03d
Zakhar Arkhipov <php-stager-1@turan-soft.ru>
2025-09-07 23:03:10 +0700

    Урок 14 завершён, сделал дататейбл и фильтрации по количеству элементов на странице и поиск

--- app/Livewire/User/Userlist.php
+++ app/Livewire/User/Userlist.php
@@ -4,6 +4,7 @@ namespace App\Livewire\User;
 
 
 use Livewire\Attributes\On;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use App\Models\User;
 use Livewire\WithPagination;
@@ -12,6 +13,37 @@ class Userlist extends Component
 {
 
     use WithPagination;
+
+    #[Url]
+    public int $limit = 10;
+
+    public array $limitList = [10, 25, 50, 100];
+
+    #[Url]
+    public string $search = '';
+
+     public function mount()
+    {
+        if (!in_array($this->limit, $this->limitList)) {
+            $this->redirectRoute('home');
+        }
+    }
+
+    public function changeLimit()
+    {
+        $this->limit = in_array($this->limit, $this->limitList) ? $this->limit : $this->limitList[0];
+        $this->resetPage();
+    }
+
+    public function updating($property, $value)
+    {
+        if ($property == 'search')
+        {
+            $this->resetPage();
+        }
+    }
+    
+
     public function deleteUser(int $id)
     {
         User::find($id)->delete();
@@ -25,9 +57,23 @@ class Userlist extends Component
 
     
 
+    
+
     public function render()
     {
-        return view('livewire.user.userlist')-> with(['users' => User::query()->with('country')->orderBy('id', 'desc')
-        ->paginate(5, pageName: 'users-page')]);
+        $users = User::query()
+        ->select('users.id', 'users.name', 'users.email', 'countries.name as country_name')
+        ->join('countries', 'users.country_id', '=', 'countries.id')
+        ->when($this->search, function ($query)
+            {
+                $query->whereAny([
+                    'users.name', 'users.email', 'countries.name'
+                ], 'like', $this->search);
+            })
+            ->orderBy('id', 'desc')
+            ->paginate($this->limit);
+        return view('livewire.user.userlist')-> with([
+            'users' => $users,
+        ]);
     }
 }
--- resources/views/livewire/user/userlist.blade.php
+++ resources/views/livewire/user/userlist.blade.php
@@ -1,20 +1,55 @@
 <div class="col-md-12">
-    
-    <ul id="users-list">
-        @forelse ($users as $user)
-            <li wire:key="{{ $user->id }}">
-            {{ $user->name }} 
-            ({{ $user->email }}) 
-            | {{ $user->country->name }}
-                | <a href="#" wire:click.prevent="deleteUser({{ $user->id }})" wire:confirm="Удалить?">Удалить</a>
-            @if ($user->avatar)
-                <img src="{{ asset("uploads/{$user->avatar}") }}" height="30px">
-            @endif
-            </li>
-        @empty
-            <p>Тут пусто...</p>
-        @endforelse
-    </ul>    
+
+    <div class="d-flex justify-content-between mb-2" id="users-list">
+
+    <div>
+         <select class="form-select" wire:model="limit" wire:change="changeLimit">
+                @foreach($limitList as $k => $v)
+                    <option @if($v == $limit) selected @endif wire:key="{{ $k }}" value="{{ $v }}">{{ $v }}</option>
+                @endforeach
+            </select>
+    </div>
+
+    <div>
+        <input type="text" class="form-control" id="search" placeholder="Поиск" wire:model.live.debounce.300ms="search">
+    </div>
+        
+    </div>
+
+    {{ $users->links() }}
+    <div class="table-responsive position-relative">
+
+        <div wire:loading style="position: absolute; width: 100%; height: 100%;
+            background: rgba(255, 255, 255, .7); text-align: center; padding-top: 20px;">
+            <div class="spinner-border" role="status" >
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+        
+
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Имя</th>
+                    <th>Email</th>
+                    <th>Страна</th>
+                    <th>Удалить</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr wire:key="{{ $user->id }}">
+                        <td>{{ $user->id }}</td>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->country_name }}</td>
+                        <td><button wire:click="deleteUser({{ $user->id }})" wire:confirm="Удалить?" class="btn btn-danger">Удалить</button></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>   
 
     {{ $users->links(data: ['scrollTo' => '#users-list']) }}
         
